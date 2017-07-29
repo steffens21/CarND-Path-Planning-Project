d57da3a026bf9b113115c620f36b6f1dc2536a21
Ubuntu <steffens21@gmail.com>
2017-07-29 20:08:37 +0000

    Added function headers in cost.cpp

--- src/cost.cpp
+++ src/cost.cpp
@@ -7,3 +7,49 @@ using namespace std;
 CostFunctions::CostFunctions() {}
 
 CostFunctions::~CostFunctions() {}
+
+
+float CostFunctions::time_diff_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+
+float CostFunctions::s_diff_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+
+float CostFunctions::d_diff_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+
+bool CostFunctions::collision_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+
+float CostFunctions::buffer_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+
+//float CostFunctions::stays_on_road_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {}
+
+//float CostFunctions::exceeds_speed_limit_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {}
+
+float CostFunctions::efficiency_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+
+float CostFunctions::max_accel_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+
+float CostFunctions::total_accel_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+
+float CostFunctions::max_jerk_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+
+float CostFunctions::total_jerk_cost(vector<double> traj, int target_vehicle, double delta, double T, vector<double> predictions) {
+
+}
+  
